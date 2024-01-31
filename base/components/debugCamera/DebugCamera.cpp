8cea77527c95fefd98971f02b7f887814d21959f
Y04Kato <k022g0046@g.neec.ac.jp>
2024-01-31 11:06:42 +0900

    フィルター整理

--- base/components/debugCamera/DebugCamera.cpp
+++ base/components/debugCamera/DebugCamera.cpp
@@ -33,6 +33,25 @@ void DebugCamera::Update() {
 	ImGui::DragFloat3("translation", viewProjection_.translation_.num, 0.1f);
 	ImGui::End();
 #endif
+
+	if (isMovingCamera == true) {
+		timer_ += timerCountr_;
+		movingEndTranslate_.num[0] += movingSpeed_.num[0];
+		movingEndTranslate_.num[1] += movingSpeed_.num[1];
+		movingEndTranslate_.num[2] += movingSpeed_.num[2];
+		if (timer_ >= 1.0f) {
+			isMovingCamera = false;
+			timer_ = 1.0f;
+		}
+		viewProjection_.translation_.num[0] = (1.0f - timer_) * movingStartTranslate_.num[0] + timer_ * movingEndTranslate_.num[0];
+		viewProjection_.translation_.num[1] = (1.0f - timer_) * movingStartTranslate_.num[1] + timer_ * movingEndTranslate_.num[1];
+		viewProjection_.translation_.num[2] = (1.0f - timer_) * movingStartTranslate_.num[2] + timer_ * movingEndTranslate_.num[2];
+		viewProjection_.rotation_.num[0] = (1.0f - timer_) * movingStartRotate_.num[0] + timer_ * movingEndRotate_.num[0];
+		viewProjection_.rotation_.num[1] = (1.0f - timer_) * movingStartRotate_.num[1] + timer_ * movingEndRotate_.num[1];
+		viewProjection_.rotation_.num[2] = (1.0f - timer_) * movingStartRotate_.num[2] + timer_ * movingEndRotate_.num[2];
+	}
+
+
 	viewProjection_.UpdateMatrix();
 }
 
@@ -45,4 +64,18 @@ void DebugCamera::ShakeCamera(int shakePower, int dividePower) {
 void DebugCamera::SetCamera(Vector3 translation, Vector3 rotation) {
 	viewProjection_.translation_ = translation;
 	viewProjection_.rotation_ = rotation;
+}
+
+void DebugCamera::MovingCamera(Vector3 translation, Vector3 rotation, float timerSpeed) {
+	timer_ = 0.0f;
+	timerCountr_ = timerSpeed;
+	movingStartTranslate_ = viewProjection_.translation_;
+	movingStartRotate_ = viewProjection_.rotation_;
+	movingEndTranslate_ = translation;
+	movingEndRotate_ = rotation;
+	isMovingCamera = true;
+}
+
+void DebugCamera::SetMovingSpeed(Vector3 moveSpeed) {
+	movingSpeed_ = moveSpeed;
 }
