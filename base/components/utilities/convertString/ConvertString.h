bce2c43603d14de6b8784beb24afa40a346d3220
Y04Kato <k022g0046@g.neec.ac.jp>
2024-02-05 12:13:37 +0900

    コメントアウト追加

--- base/components/utilities/convertString/ConvertString.h
+++ base/components/utilities/convertString/ConvertString.h
@@ -4,12 +4,16 @@
 #include <Windows.h>
 #include <vector>
 
+//string → wstring
 std::wstring ConvertString(const std::string& str);
 
+//wstring → string
 std::string ConvertString(const std::wstring& str);
 
+//string → char
 const char ConvertStringC(const std::string& str);
 
+//char → string
 std::string ConvertStringC(const char& c);
 
 void Log(const std::string& message);
