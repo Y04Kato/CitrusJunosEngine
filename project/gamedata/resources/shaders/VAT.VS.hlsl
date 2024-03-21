f9e8251ce904631846157c137e25be9780e294af
Alfort121R <reiya12121@gmail.com>
2024-03-21 20:43:26 +0900

    hlsl書き換え

--- project/gamedata/resources/shaders/VAT.VS.hlsl
+++ project/gamedata/resources/shaders/VAT.VS.hlsl
@@ -8,14 +8,15 @@ Texture2D g_VATexture;
 
 VertexShaderOutput main(VertexShaderInput input) {
 	VertexShaderOutput output;
+	
 	float32_t4x4 WorldViewProjection = mul(gViewProjectionMatrix.view, gViewProjectionMatrix.projection);
 
-	float3 vatPos = g_VATexture.Load(int3(int(In.TextureUV.x), g_Location, 0)).xyz;
+	float3 vatPos = g_VATexture.Load(int3(int(input.normal.x), g_Location, 0)).xyz;
 
-	output.position = mul(input.position, mul(gTransformationMatrix.matWorld, WorldViewProjection));
+    output.position = mul(input.position, mul(gTransformationMatrix.matWorld, (WorldViewProjection + vatPos)));
 	output.texcoord = input.texcoord;
 	output.normal = normalize(mul(input.normal, (float32_t3x3)gTransformationMatrix.WorldInverseTranspose));
 	output.worldPosition = mul(input.position, gTransformationMatrix.matWorld).xyz;
-	output.VATColor = vatPos
+    output.VATColor = vatPos;
 	return output;
 }
