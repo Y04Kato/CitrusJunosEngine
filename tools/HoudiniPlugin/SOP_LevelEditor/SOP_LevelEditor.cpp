107a118412b32913e6d75f7a9e7ef9bb3e6e0f38
Y04Kato <k022g0046@g.neec.ac.jp>
2024-04-18 12:29:12 +0900

    tools更新

--- tools/HoudiniPlugin/SOP_LevelEditor/SOP_LevelEditor.cpp
+++ tools/HoudiniPlugin/SOP_LevelEditor/SOP_LevelEditor.cpp
@@ -1,4 +1,4 @@
-﻿// NOTE: このテンプレはprotoヘッダファイルを使用する前提のもの
+// NOTE: このテンプレはprotoヘッダファイルを使用する前提のもの
 // TODO: インクルードするファイル名を書き換える
 #include "SOP_LevelEditor.hpp"
 #include "SOP_LevelEditor.proto.h" // protoヘッダを使うことが前提
@@ -19,64 +19,75 @@ using namespace SOP_LEVELEDITOR;
 const UT_StringHolder SOP_LevelEditor::theSOPTypeName("LevelEditor");
 
 // NOTE: プラグインロード時にOP_OperatorTableにカスタムSOPを登録する
-void newSopOperator(OP_OperatorTable* table){
-    table->addOperator(new OP_Operator(
-        // TODO: 必要に応じて書き換える
-        SOP_LevelEditor::theSOPTypeName,   // SOPの内部名
-        "LevelEditor",                     // SOPのラベル名
-        SOP_LevelEditor::myConstructor,    // 自身のインスタンスを返すメソッド
-        SOP_LevelEditor::buildTemplates(), // パラメータテンプレートを指定
-        0,                              // 最低限必要な入力数
-        0,                              // 入力プラグの数
-        nullptr,                        // Custom local variables (none)
-        OP_FLAG_GENERATOR));            // Flag it as generator
+void newSopOperator(OP_OperatorTable* table) {
+	table->addOperator(new OP_Operator(
+		// TODO: 必要に応じて書き換える
+		SOP_LevelEditor::theSOPTypeName,   // SOPの内部名
+		"LevelEditor",                     // SOPのラベル名
+		SOP_LevelEditor::myConstructor,    // 自身のインスタンスを返すメソッド
+		SOP_LevelEditor::buildTemplates(), // パラメータテンプレートを指定
+		0,                              // 最低限必要な入力数
+		0,                              // 入力プラグの数
+		nullptr,                        // Custom local variables (none)
+		OP_FLAG_GENERATOR));            // Flag it as generator
 }
 
 // NOTE: SOPに持たせるパラメータを定義する
 // TODO: .dsファイル、または.dsファイルと同様のルールで書かれたraw文字列を代入する
 static const char* theDsFile = R"THEDSFILE({
-        name        parameters
+    name        parameters
+    parm {
+        name    "testParameter"      // 内部パラメータ名
+        label   "testParameter" // インターフェース表示名
+        type    integer
+        default { "0" }     // パラメータのデフォルト値
+        range   { 2! 50 }   // インターフェース上での値の最低値と最大値
+                            // 2!で2以下に絶対にならないようになる
+                            // スライダーの最大値が50だが、手動で更に高く設定可能
+        export  all         // パラメータをツールボックスに表示
+                            // ノードの状態がビューポートに表示される
+    }
 }
 )THEDSFILE";
 
 // NOTE: *theDsFile で記述された内容をもとにパラメータテンプレートを生成する
 // TODO: カスタムSOPクラスを指定
 // TODO: 正しいcppファイル名に書き換える
-PRM_Template* SOP_LevelEditor::buildTemplates(){
-    static PRM_TemplateBuilder templ("SOP_LevelEditor.cpp", theDsFile);
-    return templ.templates();
+PRM_Template* SOP_LevelEditor::buildTemplates() {
+	static PRM_TemplateBuilder templ("SOP_LevelEditor.cpp", theDsFile);
+	return templ.templates();
 }
 
 // NOTE: Verbの仕組みを使うため、カスタムNodeVerbクラスを宣言
 // TODO: カスタムNodeVerbクラス名を指定
-class SOP_LevelEditorVerb : public SOP_NodeVerb{
+class SOP_LevelEditorVerb : public SOP_NodeVerb {
 public:
-    // NOTE: デフォルトコンストラクタとデストラクタ
-    // TODO: カスタムNodeVerbクラス名を指定
-    SOP_LevelEditorVerb() {}
-    virtual ~SOP_LevelEditorVerb() {}
+	// NOTE: デフォルトコンストラクタとデストラクタ
+	// TODO: カスタムNodeVerbクラス名を指定
+	SOP_LevelEditorVerb() {}
+	virtual ~SOP_LevelEditorVerb() {}
 
-    // TODO: protoヘッダで定義されているパラメータテンプレートクラスを指定
-    virtual SOP_NodeParms* allocParms() const{
-        return new SOP_LevelEditorParms();
-    }
+	// TODO: protoヘッダで定義されているパラメータテンプレートクラスを指定
+	virtual SOP_NodeParms* allocParms() const {
+		return new SOP_LevelEditorParms();
+	}
 
-    // NOTE: theVerbメンバ変数の宣言
-    // TODO: カスタムNodeVerbクラスを指定
-    static const SOP_NodeVerb::Register<SOP_LevelEditorVerb> theVerb;
+	// NOTE: theVerbメンバ変数の宣言
+	// TODO: カスタムNodeVerbクラスを指定
+	static const SOP_NodeVerb::Register<SOP_LevelEditorVerb> theVerb;
 
-    virtual UT_StringHolder name() const{
-        // TODO: カスタムSOPクラスを指定
-        return SOP_LevelEditor::theSOPTypeName;
-    }
+	virtual UT_StringHolder name() const {
+		// TODO: カスタムSOPクラスを指定
+		return SOP_LevelEditor::theSOPTypeName;
+	}
 
-    // NOTE: 編集不要
-    virtual CookMode cookMode(const SOP_NodeParms* parms) const{
-        return COOK_GENERIC;
-    }
+	// NOTE: 編集不要
+	virtual CookMode cookMode(const SOP_NodeParms* parms) const {
+		return COOK_GENERIC;
+	}
 
-    // NOTE: 編集不要
-    virtual void cook(const CookParms& cookparms) const;
+	// NOTE: 編集不要
+	virtual void cook(const CookParms& cookparms) const;
 };
 
 // NOTE: 静的なメンバ変数の定義はクラス定義の外に書く
@@ -84,12 +95,12 @@ public:
 const SOP_NodeVerb::Register<SOP_LevelEditorVerb> SOP_LevelEditorVerb::theVerb;
 
 // TODO: カスタムSOPクラスを指定
-const SOP_NodeVerb* SOP_LevelEditor::cookVerb() const{
-    return SOP_LevelEditorVerb::theVerb.get();
+const SOP_NodeVerb* SOP_LevelEditor::cookVerb() const {
+	return SOP_LevelEditorVerb::theVerb.get();
 }
 
 // NOTE: SOPのメイン処理 Verbを使う場合はSOP_NodeVerbのcookに実際の処理を委譲する
 // TODO: カスタムNodeVerbクラスを指定
-void SOP_LevelEditorVerb::cook(const SOP_NodeVerb::CookParms& cookparms) const{
-    // TODO: ノードのメイン処理はここに記述する
+void SOP_LevelEditorVerb::cook(const SOP_NodeVerb::CookParms& cookparms) const {
+	// TODO: ノードのメイン処理はここに記述する
 }
