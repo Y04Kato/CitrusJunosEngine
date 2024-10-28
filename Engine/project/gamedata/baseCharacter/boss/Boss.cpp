1270eae39492fbd6a4133ee42c10c8550c6d67d0
Alfort121R <reiya12121@gmail.com>
2024-10-28 11:39:32 +0900

    AddBike

--- Engine/project/gamedata/baseCharacter/boss/Boss.cpp
+++ Engine/project/gamedata/baseCharacter/boss/Boss.cpp
@@ -15,19 +15,49 @@ void Boss::Initialize(Model* model) {
 	//NULLポインタチェック
 	BaseCharacter::Initialize(model);
 
+	//バイクのモデル追加
+	bikeModel_.reset(Model::CreateModel("project/gamedata/resources/floor", "Floor.obj"));
+	bikeModel_->SetDirectionalLightFlag(true, 3);
 
+	bikeWorld_.Initialize();
 }
 
 void Boss::Update() {
 	//行列を定数バッファに転送
 	worldTransform_.TransferMatrix();
+	bikeWorld_.TransferMatrix();
 
+	//HPが0になったら死亡フラグをTrueにする
+	if (hp_ <= 0) {
+		isDead_ = true;
+	}
 
+	//クールタイムが0になったら攻撃を行う
+	attackTimer_--;
+	if (attackTimer_ <= 0) {
+		if (nowAttackPattern_ == 1) {
+			Attack1();
+		}
+
+		if (nowAttackPattern_ == 2) {
+			Attack2();
+		}
+	}
 
 	worldTransform_.UpdateMatrix();
+	bikeWorld_.UpdateMatrix();
 }
 
 void Boss::Draw(const ViewProjection& viewProjection) {
 	//3Dモデルを描画
 	model_->Draw(worldTransform_, viewProjection, Vector4{ 1.0f,1.0f,1.0f,1.0f });
+	bikeModel_->Draw(bikeWorld_, viewProjection, Vector4{ 1.0f,1.0f,1.0f,1.0f });
+}
+
+void Boss::Attack1() {
+
+}
+
+void Boss::Attack2() {
+
 }
--- Engine/project/gamedata/baseCharacter/boss/Boss.h
+++ Engine/project/gamedata/baseCharacter/boss/Boss.h
@@ -22,6 +22,10 @@ public:
 	//独楽を撃ちだす攻撃
 	void Attack2();
 
+	//生死フラグ
+	bool GetisDead() { return isDead_; }
+	void SetisDead(const bool flag) { isDead_ = flag; }
+
 	WorldTransform GetWorldTransform() { return worldTransform_; }
 	void SetWorldTransform(const Vector3 translation) { worldTransform_.translation_ = translation; }
 
@@ -29,4 +33,15 @@ protected:
 	const int maxHP_ = 10;//最大体力
 	const int faseChangePoint_ = 5;//体力がこの値以下になったらフェイズ2へ
 	int hp_ = maxHP_;//現在体力
+
+	std::unique_ptr<Model> bikeModel_;
+	WorldTransform bikeWorld_;
+
+	//生死フラグ
+	bool isDead_ = false;
+
+	//攻撃に関するモノ
+	const int resetTime_ = 180;//攻撃のクールタイム
+	int attackTimer_ = resetTime_;//現在のクールタイム
+	int nowAttackPattern_ = 1;//次の攻撃内容。各Attack関数の末尾の数字
 };
